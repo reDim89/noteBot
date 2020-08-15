7b4a703a94a72f8f2cc4f9abe580ad134a5580c4
Dmitry Taranov <taranovd@manychat.com>
2020-08-15 20:15:11 +0300

    first commit

---
+++ src/noteBot.py
@@ -0,0 +1,12 @@
+from telegram.ext import Updater, CommandHandler
+
+
+class NoteBot():
+    def __init__(self, token):
+        self.updater = Updater(token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
+        self.dispatcher.add_handler(CommandHandler('start', self.start))
+
+    @staticmethod
+    def start(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
